e8239d04b4c940aa510cf0ee05578f6ef7604bb6
Artem Fokin <artem@favrit.com>
2022-08-14 11:55:50 +0200

    Add "should parse geoData and send building_limits for saving" failing test

--- src/test/kotlin/com/itlbv/buildinglimitsprocessor/GeoDataServiceTest.kt
+++ src/test/kotlin/com/itlbv/buildinglimitsprocessor/GeoDataServiceTest.kt
@@ -1,3 +1,35 @@
 package com.itlbv.buildinglimitsprocessor
 
-internal class GeoDataServiceTest
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+
+internal class GeoDataServiceTest {
+
+    private val geoDataService = GeoDataService()
+
+    @Test
+    fun `should parse geoData and send building_limits for saving`() {
+        // when
+        geoDataService.parseAndSaveGeoData(GEODATA_JSON)
+
+        // then
+        verify { buildingLimitsService.save(BUILDING_LIMITS_JSON) }
+    }
+
+    companion object {
+        val BUILDING_LIMITS_JSON = """
+            {"type":"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[10.0,10.0],[20.0,10.0],[20.0,10.0],[20.0,20.0]]]}},{"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[1.0,1.0],[5.0,1.0],[5.0,1.0],[5.0,5.0]]]}}]}
+        """.trim()
+
+        val HEIGHT_PLATEAUS = """
+            {"type":"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[15.0,15.0,3.0],[25.0,15.0,3.0],[25.0,15.0,3.0],[25.0,25.0,3.0]]]}},{"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[1.0,1.0,2.0],[5.0,1.0,2.0],[5.0,1.0,2.0],[5.0,5.0,2.0]]]}}]}
+        """.trimIndent()
+
+        val GEODATA_JSON = """
+            {
+                "building_limits": $BUILDING_LIMITS_JSON,
+                "height_plateaus": $HEIGHT_PLATEAUS
+            }
+        """.trim()
+    }
+}
