f2e64b159936a1776976d03a6d5eb2a8b3f9af87
Artem Fokin <artem@favrit.com>
2022-08-15 08:50:47 +0200

    Add failing "should save building limits" test

--- src/test/kotlin/com/itlbv/buildinglimitsprocessor/BuildingLimitsServiceTest.kt
+++ src/test/kotlin/com/itlbv/buildinglimitsprocessor/BuildingLimitsServiceTest.kt
@@ -1,30 +1,38 @@
 package com.itlbv.buildinglimitsprocessor
 
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verifyAll
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import java.math.BigDecimal
 
 internal class BuildingLimitsServiceTest {
 
-    private val buildingLimitsRepository = mockk<BuildingLimitsRepository>()
+    private val buildingLimitsRepository = mockk<BuildingLimitsRepository>(relaxed = true)
 
     @Test
     fun `should save building limits`() {
         // given
-        val buildingLimitsService = BuildingLimitsService()
-        val expectedBuildingLimit1 = BuildingLimit(listOf(
-            arrayOf(10.0, 10.0),
-            arrayOf(20.0, 10.0),
-            arrayOf(10.0, 20.0),
-            arrayOf(20.0, 20.0),
-        ))
-        val expectedBuildingLimit2 = BuildingLimit(listOf(
-            arrayOf(1.0, 5.0),
-            arrayOf(5.0, 1.0),
-            arrayOf(1.0, 5.0),
-            arrayOf(5.0, 5.0),
-        ))
+        val buildingLimitsService = BuildingLimitsService(buildingLimitsRepository)
+
+        val expectedBuildingLimit1 = BuildingLimit(
+            points = setOf(
+                Pair(BigDecimal("10.0"), BigDecimal("10.0")),
+                Pair(BigDecimal("20.0"), BigDecimal("10.0")),
+                Pair(BigDecimal("10.0"), BigDecimal("20.0")),
+                Pair(BigDecimal("20.0"), BigDecimal("20.0")),
+            )
+        )
+        val expectedBuildingLimit2 = BuildingLimit(
+            points = setOf(
+                Pair(BigDecimal("1.0"), BigDecimal("1.0")),
+                Pair(BigDecimal("5.0"), BigDecimal("1.0")),
+                Pair(BigDecimal("1.0"), BigDecimal("5.0")),
+                Pair(BigDecimal("5.0"), BigDecimal("5.0")),
+            )
+        )
+
+        every { buildingLimitsRepository.save(any()) } returns BuildingLimit(points = setOf())
 
         // when
         buildingLimitsService.save(BUILDING_LIMITS_JSON)
@@ -39,67 +47,22 @@ internal class BuildingLimitsServiceTest {
     companion object {
         private val BUILDING_LIMITS_JSON = """
             {
-                "type": "FeatureCollection",
-                "features":
-                [
+                "type": "GeometryCollection",
+                "geometries": [
                     {
-                        "type": "Feature",
-                        "geometry":
-                        {
-                            "type": "Polygon",
-                            "coordinates":
-                            [
-                                [
-                                    [
-                                        10.0,
-                                        10.0
-                                    ],
-                                    [
-                                        20.0,
-                                        10.0
-                                    ],
-                                    [
-                                        10.0,
-                                        20.0
-                                    ],
-                                    [
-                                        20.0,
-                                        20.0
-                                    ]
-                                ]
-                            ]
-                        }
+                        "type": "Polygon",
+                        "coordinates": [
+                            [[10.0, 10.0], [20.0, 10.0], [10.0, 20.0], [20.0, 20.0]]
+                        ]
                     },
                     {
-                        "type": "Feature",
-                        "geometry":
-                        {
-                            "type": "Polygon",
-                            "coordinates":
-                            [
-                                [
-                                    [
-                                        1.0,
-                                        1.0
-                                    ],
-                                    [
-                                        5.0,
-                                        1.0
-                                    ],
-                                    [
-                                        1.0,
-                                        5.0
-                                    ],
-                                    [
-                                        5.0,
-                                        5.0
-                                    ]
-                                ]
-                            ]
-                        }
+                        "type": "Polygon",
+                        "coordinates": [
+                            [[1.0, 1.0], [5.0, 1.0], [1.0, 5.0], [5.0, 5.0]]
+                        ]
                     }
                 ]
             }
-        """.trim()
+        """.trimIndent()
     }
 }
